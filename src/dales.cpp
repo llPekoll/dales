4ef71b0677169878be40ff5ce854672d830f652f
llPekoll <yoyo.mepa@gmail.com>
2015-05-10 10:33:57 +0700

    upadte

---
+++ src/dales.cpp
@@ -0,0 +1,9 @@
+//
+//  dales.cpp
+//  Intercative_floor
+//
+//  Created by peko on 5/10/15.
+//
+//
+
+#include "dales.h"
---
+++ src/dales.h
@@ -0,0 +1,30 @@
+//
+//  dales.h
+//  Intercative_floor
+//
+//  Created by peko on 5/10/15.
+//
+//
+
+#ifndef __Intercative_floor__dales__
+#define __Intercative_floor__dales__
+
+#include <stdio.h>
+#include "ofMain.h"
+
+class dales {
+public:
+    
+    void update();
+    void draw();
+    
+    float x,y;
+    
+    dales();
+    
+private:
+    
+    
+};
+
+#endif /* defined(__Intercative_floor__dales__) */
